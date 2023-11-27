10391e807bebf723f520d5ca9319b2950716cc75
Alyer77777 <romeotamere@gmail.com>
2023-11-27 16:03:13 +0100

    first commit

---
+++ src/Controller/OrdinateurController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrdinateurController extends AbstractController
+{
+    #[Route('/ordinateur', name: 'ordinateur')]
+    public function index(): Response
+    {
+        return $this->render('ordinateur/ordinateur.html.twig', [
+            'controller_name' => 'OrdinateurController',
+        ]);
+    }
+}
